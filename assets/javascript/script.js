8b118d07e026954dba37500ce6e60fe8bb47c6ee
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-06-27 21:23:53 +0100

    added 1 blog post

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,10 +1,36 @@
 $(document).ready(function () {
 
-    // Takes you to archives. page
 
-    setTimeout(() => {
-        $('.to_archives_btn').click(function () {
-            location.href = "https://delroybrown28.github.io/archive/index.html";
+
+    // Takes you to "Go back to Africa" article
+    $('.back_to_africa').click(function () {
+        $('.blog_posts').animate({
+            left: '200px',
+            opacity: '0'
+        }, function () {
+            $('.padding_wrap').fadeOut('fast', function () {
+                $('.backToAfrica_wrap').fadeIn('slow', function () {
+                    $('.to_articles_btn').animate({
+                        opacity: '1'
+                    }, 100)
+                })
+            });
         })
-    }, 5000);
+
+    })
+
+
+    // Takes you back to all articles
+    $('.to_articles_btn').click(function () {
+        $('.backToAfrica_wrap').fadeOut('fast', function () {
+            $('.padding_wrap').fadeIn('slow', function () {
+                $('.blog_posts').animate({
+                    left: '0',
+                    opacity: '1'
+                })
+            });
+        });
+    })
+
+
 })
